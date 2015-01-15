24d60d99a4568a1e6d7cd0939b0fb89259e271ff
Shinichi <shinichi.tomita@gmail.com>
2015-01-15 17:48:01 +0900

    change release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Releasing built result to GitHub Page"
-git checkout -b release
-git add out/
-git commit -am "Generate Website from docpad"
 git config user.name ${GIT_NAME}
 git config user.email ${GIT_EMAIL}
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+git checkout -b release
+git add out/
+git commit -am "Generate Website from docpad"
 git remote add pages ${GITHUB_PAGES_GIT_URL}
 git push pages `git subtree split --prefix out release`:master --force
 git remote remove pages
