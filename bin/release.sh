00a018179e01ca22657893e1d0dbd37bf1fc2690
Shinichi <shinichi.tomita@gmail.com>
2015-01-15 17:32:29 +0900

    add build script for travis

---
+++ bin/release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git checkout -b release
+git add out/
+git commit -am "Generate Website from docpad"
+git config user.name ${GIT_NAME}
+git config user.email ${GIT_EMAIL}
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+git remote add pages ${GITHUB_PAGES_GIT_URL}
+git push pages `git subtree split --prefix out release`:master --force
+git remote remove pages
+git checkout master
+
